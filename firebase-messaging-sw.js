4532fc9ada1198d074c6a8a21334edb92c15088a
felixmberner-rgb <felix.m.berner@icloud.com>
2026-04-17 23:05:27 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,25 +1,12 @@
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
 
-// Liest die Zugangsdaten dynamisch aus dem Link, den die App aufruft
 const urlParams = new URLSearchParams(self.location.search);
-
 firebase.initializeApp({
   apiKey: urlParams.get('apiKey'),
   projectId: urlParams.get('projectId'),
   messagingSenderId: urlParams.get('senderId'),
   appId: urlParams.get('appId')
 });
-
 const messaging = firebase.messaging();
-
-// Hintergrund-Nachrichten verarbeiten (wird angezeigt, wenn die App geschlossen ist)
-messaging.onBackgroundMessage((payload) => {
-  console.log('Background message received: ', payload);
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
+// Mehr braucht diese Datei nicht! Firebase übernimmt den Rest automatisch.
